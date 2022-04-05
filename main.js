ab417e46155952b874c42c832da1d0cdb4908c18
ijeomaese <ijeomaodinko@gmail.com>
2022-04-05 08:40:48 -0400

    Fix linters error

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-const bars = document.getElementById('pop')
-const closeMenu = document.querySelector(".close")
-const popUp = document.querySelector(".up-menu")
+const bars = document.getElementById('pop');
+const closeMenu = document.querySelector('.close');
+const popUp = document.querySelector('.up-menu');
 
+bars.addEventListener('click', () => {
+ popUp.classList.add('pop-up');
+});
 
-bars.addEventListener("click", () => {
-    popUp.classList.add('pop-up')
-})
+popUp.addEventListener('click', () => { 
+ popUp.classList.remove('pop-up');
+});
 
-popUp.addEventListener("click", () => { 
-    popUp.classList.remove('pop-up')
-}) 
-
-closeMenu.addEventListener("click", () => {
-    popUp.style.display = "none"
-})
+closeMenu.addEventListener('click', () => {
+ popUp.style.display = 'none';
+});
