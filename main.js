63061b51069b762698b30962af96c61cf257d042
ijeomaese <ijeomaodinko@gmail.com>
2022-04-05 08:49:50 -0400

    Fix linters error

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const closeMenu = document.querySelector('.close');
 const popUp = document.querySelector('.up-menu');
 
 bars.addEventListener('click', () => {
- popUp.classList.add('pop-up');
+  popUp.classList.add('pop-up');
 });
 
-popUp.addEventListener('click', () => { 
- popUp.classList.remove('pop-up');
+popUp.addEventListener('click', () => {
+  popUp.classList.remove('pop-up');
 });
 
 closeMenu.addEventListener('click', () => {
- popUp.style.display = 'none';
+  popUp.style.display = 'none';
 });
