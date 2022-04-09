800ce753dd6be2e394dc7078fb032df087abdbd0
ijeomaese <ijeomaodinko@gmail.com>
2022-04-09 11:44:09 -0400

    Add some style correction

--- main.js
+++ main.js
@@ -21,8 +21,8 @@ const modalObj = {
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     img: 'img/Portfolio1-big.svg',
     tech: ['html', 'css', 'javaScript'],
-    live: 'https://enekwestanley.github.io/Portifolio-page/',
-    source: 'https://github.com/Enekwestanley/Portifolio-page',
+    live: 'https://ijeomaodinko.github.io/Portifolio-page/',
+    source: 'https://github.com/ijeomaodinko/Portifolio-page',
   },
   modal2: {
     title: 'Multi-Post Stories',
@@ -30,8 +30,8 @@ const modalObj = {
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     img: 'img/Portfolio2-big.svg',
     tech: ['html', 'css', 'javaScript'],
-    live: 'https://enekwestanley.github.io/Portifolio-page/',
-    source: 'https://github.com/Enekwestanley/Portifolio-page',
+    live: 'https://ijeomaodinko.github.io/Portifolio-page/',
+    source: 'https://github.com/ijeomaodinko/Portifolio-page',
   },
   modal3: {
     title: 'Tonic',
@@ -39,8 +39,8 @@ const modalObj = {
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     img: 'img/Portfolio3.svg',
     tech: ['html', 'css', 'javaScript'],
-    live: 'https://enekwestanley.github.io/Portifolio-page/',
-    source: 'https://github.com/Enekwestanley/Portifolio-page',
+    live: 'https://ijeomaodinko.github.io/Portifolio-page/',
+    source: 'https://github.com/ijeomaodinko/Portifolio-page',
   },
   modal4: {
     title: 'Multi-Post Stories',
@@ -48,8 +48,8 @@ const modalObj = {
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     img: 'img/Portfolio4-big.svg',
     tech: ['html', 'css', 'javaScript'],
-    live: 'https://enekwestanley.github.io/Portifolio-page/',
-    source: 'https://github.com/Enekwestanley/Portifolio-page',
+    live: 'https://ijeomaodinko.github.io/Portifolio-page/',
+    source: 'https://github.com/ijeomaodinko/Portifolio-page',
   },
 };
 
